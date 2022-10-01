74ea3e59f2edc865bb0959d2ff52cef7a20d2bdf
Raees <muhammadraeesahmed38@gmail.com>
2022-10-01 23:18:20 +0500

    done splash screen with lottie animation

---
+++ app/src/main/java/com/example/noteapp/SplashActivity.java
@@ -0,0 +1,44 @@
+package com.example.noteapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    Handler handler;
+    LottieAnimationView lottieAnimationView;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        lottieAnimationView=findViewById(R.id.animationView);
+        textView=findViewById(R.id.textView);
+
+
+        // hide action bar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        lottieAnimationView.animate().translationX(2000).setDuration(2000).setStartDelay(2900);
+//        textView.animate().translationX(-1400).setDuration(2700).setStartDelay(0);
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
+}
