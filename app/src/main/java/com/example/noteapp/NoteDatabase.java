37928055d55b838bc017ead06e3dcb2e2e3ba5b3
Raees <muhammadraeesahmed38@gmail.com>
2022-10-02 00:02:01 +0500

    setup room db class

---
+++ app/src/main/java/com/example/noteapp/NoteDatabase.java
@@ -0,0 +1,22 @@
+package com.example.noteapp;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+
+// adding annotation for our database entities and db version.
+@Database(entities = {Note.class},version = 1)
+public abstract class NoteDatabase extends RoomDatabase {
+
+    private  static NoteDatabase instance;
+    public static  synchronized  NoteDatabase getInstance(Context context){
+        if(instance==null){
+            instance=Room.databaseBuilder(context.getApplicationContext(),NoteDatabase.class
+                    ,"note_database").fallbackToDestructiveMigration().build();
+        }
+        return instance;
+    }
+}
